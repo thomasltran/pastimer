d689d0d88daa8c20c26d6b73d14a1df873021942
chengJazhua <chengjazhua@gmail.com>
2021-12-02 11:05:38 -0500

    transferred from jgrasp

--- desktop/src/com/pastimer/desktop/GameScreen.java
+++ desktop/src/com/pastimer/desktop/GameScreen.java
@@ -9,12 +9,16 @@ import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.TimeUtils;
+
+import org.lwjgl.opengl.Display;
 
 public class GameScreen implements Screen {
     final Drop game;
@@ -28,6 +32,14 @@ public class GameScreen implements Screen {
     Array<Rectangle> raindrops;
     long lastDropTime;
     int dropsGathered;
+
+    Rectangle platform;
+    Rectangle ball;
+    int[][] boxes;
+    int xChange;
+    double angle = 270;
+    int length = Display.getWidth();
+    int height = Display.getHeight();
 
     public GameScreen(final Drop game) {
         this.game = game;
@@ -44,7 +56,7 @@ public class GameScreen implements Screen {
         // create the camera and the SpriteBatch
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 480);
-
+/*
         // create a Rectangle to logically represent the bucket
         bucket = new Rectangle();
         bucket.x = 800 / 2 - 64 / 2; // center the bucket horizontally
@@ -56,7 +68,10 @@ public class GameScreen implements Screen {
         // create the raindrops array and spawn the first raindrop
         raindrops = new Array<Rectangle>();
         spawnRaindrop();
-
+*/
+        boxes = new int [5][10];
+        ball = new Rectangle(400, 150, 30, 30);
+        platform = new Rectangle(Display.getWidth()/2, 50, 200, 20);
     }
 
     private void spawnRaindrop() {
@@ -87,6 +102,42 @@ public class GameScreen implements Screen {
         // begin a new batch and draw the bucket and
         // all drops
         game.batch.begin();
+
+        
+
+
+        for (int i = 0; i < 5; i++){
+            for (int j = 0; j < 10; j++){
+                if (length/10*j + 15 > ball.x && ball.x > length/10*j - 15 && height/2+height/10*(i)+15 > ball.y && ball.y > height/2+height/10*(i) -15)
+                    boxes[i][j]++;
+                if (boxes[i][j] == 0){
+                    game.batch.draw(bucketImage, length/10*j, height/2+height/10*(i));
+                }
+            
+            }
+        
+        }
+        game.font.draw(game.batch, "HI" , 0, 480);
+        game.batch.draw(bucketImage, platform.x, platform.y, platform.width,platform.height);
+        game.batch.draw(bucketImage, ball.x, ball.y, ball.width, ball.height);
+        game.batch.end();
+
+        if (Gdx.input.isKeyPressed(Keys.LEFT))
+            platform.x -= 200 * Gdx.graphics.getDeltaTime();
+            
+        if (Gdx.input.isKeyPressed(Keys.RIGHT))
+            platform.x += 200 * Gdx.graphics.getDeltaTime();
+
+        if (platform.x < 0)
+            platform.x = 0;
+
+        if (platform.x > 800 - 100)
+            platform.x = 800 - 100;
+
+        collide();
+        ball.x += 5 * Math.cos(Math.toRadians(angle));
+        ball.y += 5 * Math.sin(Math.toRadians(angle));
+        /*
         game.font.draw(game.batch, "Drops Collected: " + dropsGathered, 0, 480);
         game.batch.draw(bucketImage, bucket.x, bucket.y, bucket.width, bucket.height);
         for (Rectangle raindrop : raindrops) {
@@ -130,8 +181,57 @@ public class GameScreen implements Screen {
                 dropSound.play();
                 iter.remove();
             }
-        }
+        }*/
     }
+
+    public boolean collide(){
+        for (int i = 0; i < boxes.length; i++){
+           for (int j = 0; j < boxes[0].length; j++){
+              int size = length/20;
+              if (boxes[i][j] == 0 && distance(j*length/10+size, height/2 + (i)*height/10+size, (int)(ball.x+ball.height/2), (int) (ball.y+ball.height/2)) < size+5){
+                 boxes[i][j]++;
+                 calcAng(j*length/10, height/2 + (i)*height/10+size, size);
+                 //angle = 270;
+                 return true;
+              }
+                 
+           }
+        }
+           for (int i = 0; i < platform.x; i++){
+              if (distance((int) (platform.x+i), (int) platform.y, (int) ball.x, (int) ball.y) < 15){
+                 calcAng();
+                 //angle = 90;
+                 return true;
+              }
+           }
+        if (ball.getX() < 0 || ball.getX() > length ){
+           angle = Math.abs(180 - angle);
+        }
+        if (ball.getY() < 0 || ball.getY() > height){
+           angle = Math.abs(360 - angle);
+        }
+        return false;
+     }
+
+     public void calcAng(){
+        double midx = (double)(platform.x+platform.width/2 - ball.x)/(double)(platform.width/2);
+        double bounce = midx * 7*Math.PI/12;
+        angle =  bounce*180/Math.PI;
+     }
+
+     public void calcAng(int x, int y, int size){
+        if (ball.y > y-2)
+           angle = 360 - angle;
+        else
+           angle = 180-angle;
+     }
+  
+  
+
+    public static double distance(int x1, int y1, int x2, int y2){
+      return Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+     }
+
 
     @Override
     public void resize(int width, int height) {
