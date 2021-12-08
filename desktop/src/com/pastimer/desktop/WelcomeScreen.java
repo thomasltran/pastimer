1f1a2a8101dcc2be7676e1ce06ed945e61bf0643
Thomas Tran <thomasltran42@gmail.com>
2021-12-08 11:06:29 -0500

    WelcomeScreen changes

--- desktop/src/com/pastimer/desktop/WelcomeScreen.java
+++ desktop/src/com/pastimer/desktop/WelcomeScreen.java
@@ -1,7 +1,5 @@
 package com.pastimer.desktop;
 
-import com.rafaskoberg.gdx.typinglabel.utils.*;
-import com.rafaskoberg.gdx.typinglabel.effects.*;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
@@ -12,6 +10,7 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.*;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
@@ -25,15 +24,9 @@ public class WelcomeScreen extends ApplicationAdapter {
     @Override
     public void create(){
         stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
         skin = new Skin(Gdx.files.internal("test1.json"));
-        TypingLabel text = new TypingLabel("{WAIT=1.0}{GRADIENT}{SPEED=0.15}pastimer", skin, "title");//need to add in a delay
-        text.setPosition(Gdx.graphics.getWidth()/2-133, Gdx.graphics.getHeight()/2);
-        stage.addActor(text);
-        Texture texture = new Texture(Gdx.files.internal("peachLogo.png"));
-        Image image = new Image(texture);
-        image.setPosition(Gdx.graphics.getWidth()/2-133, Gdx.graphics.getHeight()/2+30);
-        stage.addActor(image);
-        Gdx.input.setInputProcessor(stage); //handles mouse/keyboard
+        this.setUp();        
     }
     @Override
     public void render(){
@@ -41,5 +34,29 @@ public class WelcomeScreen extends ApplicationAdapter {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.act();
         stage.draw();
+    }
+
+    @Override
+    public void dispose(){
+        stage.dispose();
+    }
+
+    public void setUp(){
+        TypingLabel text = new TypingLabel("{WAIT=1.0}{GRADIENT}{SPEED=0.15}pastimer", skin, "title");
+        Texture texture = new Texture(Gdx.files.internal("peachLogo.png"));
+        Image image = new Image(texture);
+        MoveToAction position = new MoveToAction();
+
+        text.setPosition(Gdx.graphics.getWidth()/2-133-10, Gdx.graphics.getHeight()/2);
+        image.setPosition(Gdx.graphics.getWidth()/2+133, Gdx.graphics.getHeight()/2);
+        image.setSize(image.getWidth()/3, image.getHeight()/3);
+
+        position.setPosition(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
+        position.setDuration(5);
+
+        text.addAction(position);
+
+        stage.addActor(text);
+        stage.addActor(image);
     }
 }
